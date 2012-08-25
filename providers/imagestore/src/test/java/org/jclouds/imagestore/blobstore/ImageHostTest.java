ccd10ceee51387c93cf6219be55dc9be7ebbe5b1
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-08-25 16:14:28 +0200

    [MOD] FileHosterTest working now

--- providers/imagestore/src/test/java/org/jclouds/imagestore/blobstore/ImageHostTest.java
+++ providers/imagestore/src/test/java/org/jclouds/imagestore/blobstore/ImageHostTest.java
@@ -7,6 +7,11 @@ import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.jclouds.imagestore.blobstore.imagehoster.file.ImageHostFile;
 import org.testng.annotations.DataProvider;
@@ -19,6 +24,10 @@ import com.google.common.io.Files;
  * 
  */
 public class ImageHostTest {
+
+    private final static String SET1 = "set1";
+    private final static String IMAGE1 = "image1";
+    private final static String IMAGE2 = "image2";
 
     /**
      * Test method for {@link org.jclouds.imagestore.blobstore.ImageHost#ImageHostFile(java.io.File)} and
@@ -36,9 +45,36 @@ public class ImageHostTest {
      *            to be tested with
      * @param pHandlers
      *            to be tested with
+     * @throws IOException
      */
     @Test(dataProvider = "instantiateHosts")
-    public void testImageHost(Class<ImageHost> clazz, ImageHost[] pHandlers) {
+    public void testImage(Class<ImageHost> clazz, ImageHost[] pHandlers) throws IOException {
+        BufferedImage[] images = createBufferedImage();
+
+        for (ImageHost host : pHandlers) {
+            assertFalse(host.imageExists(SET1, IMAGE1));
+            assertFalse(host.imageSetExists(SET1));
+            assertTrue(host.createImageSet(SET1));
+            host.uploadImage(SET1, IMAGE1, images[0]);
+            assertTrue(host.imageExists(SET1, IMAGE1));
+            BufferedImage img = host.downloadImage(SET1, IMAGE1);
+            // assertTrue(img.getGraphics().hashCode() == images[0].getGraphics().hashCode());
+            host.deleteImage(SET1, IMAGE1);
+            assertFalse(host.imageExists(SET1, IMAGE1));
+            host.uploadImage(SET1, IMAGE1, images[0]);
+            host.uploadImage(SET1, IMAGE2, images[1]);
+            assertTrue(host.imageExists(SET1, IMAGE1));
+            assertTrue(host.imageExists(SET1, IMAGE2));
+            host.clearImageSet(SET1);
+            assertFalse(host.imageExists(SET1, IMAGE1));
+            assertFalse(host.imageExists(SET1, IMAGE2));
+            host.uploadImage(SET1, IMAGE1, images[0]);
+            host.uploadImage(SET1, IMAGE2, images[1]);
+            assertTrue(host.imageExists(SET1, IMAGE1));
+            assertTrue(host.imageExists(SET1, IMAGE2));
+            host.deleteImageSet(SET1);
+            assertFalse(host.imageSetExists(SET1));
+        }
 
     }
 
@@ -54,13 +90,12 @@ public class ImageHostTest {
      */
     @Test(dataProvider = "instantiateHosts")
     public void testImageHostSets(Class<ImageHost> clazz, ImageHost[] pHandlers) {
-        String set1 = "set1";
         for (ImageHost host : pHandlers) {
-            assertFalse(host.imageSetExists(set1));
-            assertTrue(host.createImageSet(set1));
-            assertTrue(host.imageSetExists(set1));
-            host.deleteImageSet(set1);
-            assertFalse(host.imageSetExists(set1));
+            assertFalse(host.imageSetExists(SET1));
+            assertTrue(host.createImageSet(SET1));
+            assertTrue(host.imageSetExists(SET1));
+            host.deleteImageSet(SET1);
+            assertFalse(host.imageSetExists(SET1));
         }
     }
 
@@ -82,9 +117,19 @@ public class ImageHostTest {
         return returnVal;
     }
 
-    private static BufferedImage createBufferedImage() {
-        
-        
-        
+    private static BufferedImage[] createBufferedImage() throws IOException {
+        final File imageFile =
+            new File("src" + File.separator + "test" + File.separator + "resources" + File.separator
+                + "pictures");
+        BufferedImage[] returnVals = new BufferedImage[imageFile.listFiles().length];
+        int i = 0;
+        for (File file : imageFile.listFiles()) {
+            FileInputStream fis = new FileInputStream(file);
+            returnVals[i] = ImageIO.read(fis);
+            fis.close();
+            i++;
+        }
+        return returnVals;
+
     }
 }
