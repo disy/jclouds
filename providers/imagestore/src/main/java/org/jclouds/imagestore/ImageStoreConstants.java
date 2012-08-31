982a100ab20bf1f2e3ef78c425cf52c2bbadf254
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-08-31 08:42:46 +0200

    [ADD] Constancts for injecting params to constructors [MOD] added fb-lib again

---
+++ providers/imagestore/src/main/java/org/jclouds/imagestore/ImageStoreConstants.java
@@ -0,0 +1,9 @@
+package org.jclouds.imagestore;
+
+public class ImageStoreConstants {
+
+    public static final String PROPERTY_IMAGEHOSTER = "jclouds.imagestore.imagehoster";
+
+    public static final String PROPERTY_BYTEPAINTER = "jclouds.imagestore.imagegenerator";
+
+}
